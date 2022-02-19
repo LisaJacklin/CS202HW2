e85ea15a593d4dd2c70b3f618635489405087681
Lisa Jacklin <lisaloveslola09@outlook.com>
2022-02-19 11:28:37 -0900

    lowercaseletter.cpp requirements included

--- LowerCaseLetter/LowerCaseLetter.cpp
+++ LowerCaseLetter/LowerCaseLetter.cpp
@@ -5,6 +5,15 @@
 
 #include <iostream>
 
+/*
+Requirements:
+1. include void to_lower(char* s) ( replaces all upper case to lower case
+
+note: don't use any standard library functions
+- if you find a char w value 0 you are at the end
+
+*/
+
 int main()
 {
     std::cout << "Hello World!\n";
