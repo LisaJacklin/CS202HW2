d5a345bd2477700112788fe0a8ebe19a9f5bee54
Lisa Jacklin <lisaloveslola09@outlook.com>
2022-02-19 12:57:00 -0900

    memorylayout.cpp void printpointer created..

--- MemoryLayout/MemoryLayout.cpp
+++ MemoryLayout/MemoryLayout.cpp
@@ -4,26 +4,30 @@
 // 2/17/2022
 
 #include <iostream>
+using std::cout;
+using std::endl;
 
-
+#include <stdio.h>
 /*
 Requirements:
 - write a programs telling the order that different storage is layed out in memory
-
-code given:
-void printPounter (int i) {
-int* iptr = &i;
-//the unary * operator dereferences the pointer
-std::cout << "i = " << &iptr << endl;
-std::cout << "&i = " << iptr << endl;
-}
 
 - code, statoic data, free store, stack
 
 */
 
+//code given :
+void printPointer(int i) {
+    int* iptr = &i;
+    //the unary * operator dereferences the pointer
+    std::cout << "i = " << &iptr << endl;
+    std::cout << "&i = " << iptr << endl;
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "start of main" << endl;
+
+    cout << "end of main" << endl;
 }
 
