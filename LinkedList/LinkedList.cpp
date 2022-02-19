d55b299268974dc3f05a2be00febfe33cbb7e730
Lisa Jacklin <lisaloveslola09@outlook.com>
2022-02-19 10:56:34 -0900

    libnkedlist.cpp requrements added

--- LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList.cpp
@@ -5,6 +5,34 @@
 
 #include <iostream>
 
+/*
+Requirements:
+1. create a struct called Value ( rename and add 6 fields to)
+ - note these files test a queue, stack , insertion and search using linked list
+
+ Following operations:
+ Queue: first in First out
+    - push a val to the back of the list
+    - pop a value from the front of the list
+
+Stack: last in first out
+    - push a value to the back of the list
+    - pop a value from the back of the lsit
+
+Insert and Find:
+- keep list in sorted order (use some field of value)
+- search for a value in the list
+- insert a value at the appropriate place in the list
+
+print the list out.
+
+
+Notes: 
+-use linked list class from STL (header <list>)
+- in the main .cpp use CATCH.HPP to test class
+- for Value Class, might want to base it on a them...
+
+*/
 int main()
 {
     std::cout << "Hello World!\n";
